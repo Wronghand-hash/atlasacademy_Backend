a39a6b9f76be4eb6db12269745a55383596f481f
Greez <imgreez@protonmail.com>
2023-08-30 09:46:47 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
       cookie: {
         secure: true,
         httpOnly: true,
-        maxAge: 60000,
+        maxAge: 60000 * 5,
         sameSite: 'none',
         domain: 'auth.atlasacademy.ir',
       },
