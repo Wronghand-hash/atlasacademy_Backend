fea210a655bebf1ed98d4d47344d7004b38bcca6
Greez <imgreez@protonmail.com>
2023-09-05 09:10:43 +0330

    update db

--- prisma/migrations/20230805065000_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "CreativityTestResult" INTEGER,
-ADD COLUMN     "IQTestResult" INTEGER;
--- prisma/migrations/20230806142646_update_field/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `IQTestResult` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "IQTestResult",
-ADD COLUMN     "TorrenceTestResult" INTEGER;
