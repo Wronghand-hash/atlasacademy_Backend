0583b9b0d226c31255c9fc1b562d6b8e88597d4f
Greez <imgreez@protonmail.com>
2024-01-16 11:46:22 +0330

    updates

--- src/registrations/dto/CoopRequestDto.ts
+++ src/registrations/dto/CoopRequestDto.ts
@@ -2,9 +2,18 @@
 
 import { IsNotEmpty } from 'class-validator';
 
-export class RegistrationDto {
+export class CoopRequestDto {
   @IsNotEmpty({ message: 'لطفا نام کامل خود را وارد نمایید' })
   fullName: string;
+
+  @IsNotEmpty({ message: 'لطفا نام پدر خود را وارد نمایید' })
+  fatherName: string;
+
+  @IsNotEmpty({ message: 'لطفا کد ملی خود را وارد نمایید' })
+  personalId: string;
+
+  @IsNotEmpty({ message: 'لطفا وضعیت تاهل خود را وارد نمایید' })
+  status: string;
 
   @IsNotEmpty({ message: 'لطفا تیتر شماره همراه خود را وارد نمایید' })
   phoneNumber: string;
@@ -12,8 +21,8 @@ export class RegistrationDto {
   @IsNotEmpty({ message: 'لطفا تیتر شماره ثابت خود را وارد نمایید' })
   lineNumber: string;
 
-  @IsNotEmpty({ message: 'لطفا عنوان درخواست را وارد نمایید' })
-  category: string;
+  @IsNotEmpty({ message: 'لطفا محل تولد خود را وارد نمایید' })
+  birthPlace: string;
 
   @IsNotEmpty({ message: 'لطفا تاریخ تولد خود را وارد نمایید' })
   birthDate: string;
--- src/registrations/registrations.service.ts
+++ src/registrations/registrations.service.ts
@@ -1,26 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegistrationDto } from './dto/RegistrationDto';
+import { CoopRequestDto } from './dto/CoopRequestDto';
 
 @Injectable()
 export class RegistrationsService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getRegistrations() {
-    // const registrations = await this.prismaService.registrations.findMany({});
-    // return { registrations: registrations };
+    const registrations = await this.prismaService.registrations.findMany({});
+    return { registrations: registrations };
   }
 
   async addRegistration(dto: RegistrationDto) {
-    // const registration = await this.prismaService.registrations.create({
-    //   data: {
-    //     fullname: dto.fullName,
-    //     phone_number: dto.phoneNumber,
-    //     category: dto.category,
-    //     birth_date: dto.birthDate,
-    //     address: dto.address,
-    //   },
-    // });
-    // return { msg: 'درخواست اضافه شد', registration: registration };
+    const registration = await this.prismaService.registrations.create({
+      data: {
+        fullname: dto.fullName,
+        phone_number: dto.phoneNumber,
+        category: dto.category,
+        birth_date: dto.birthDate,
+        address: dto.address,
+        line_number: dto.lineNumber,
+      },
+    });
+    return { msg: 'درخواست اضافه شد', registration: registration };
+  }
+
+  async getRequests() {
+    const requests = await this.prismaService.colleages.findMany({});
+    return { requests: requests };
+  }
+
+  async addRequest(dto: CoopRequestDto) {
+    const requests = await this.prismaService.colleages.create({
+      data: {
+        fullname: dto.fullName,
+        father_name: dto.fatherName,
+        personal_id: dto.personalId,
+        status: dto.status,
+        phone_number: dto.phoneNumber,
+        category: 'مدرس',
+        birth_date: dto.birthDate,
+        address: dto.address,
+        birth_place: dto.birthPlace,
+      },
+    });
+    return { msg: 'درخواست اضافه شد', requests: requests };
   }
 }
